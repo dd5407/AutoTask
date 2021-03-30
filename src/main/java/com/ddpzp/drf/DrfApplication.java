c4181a482616a3548f3b7dc2f8caa9bd7b3613ec
smileD <476700530@qq.com>
2021-03-30 22:08:26 +0800

    init

---
+++ src/main/java/com/ddpzp/drf/DrfApplication.java
@@ -0,0 +1,13 @@
+package com.ddpzp.drf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddpzp/drf/DrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddpzp.drf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
