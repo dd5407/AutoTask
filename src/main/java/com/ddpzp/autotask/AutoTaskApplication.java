ee735f98520e69639ef2e775f993ac3f657aba9c
smileD <476700530@qq.com>
2021-03-31 00:13:47 +0800

    idea push test

--- src/main/java/com/ddpzp/autotask/AutoTaskApplication.java
+++ src/main/java/com/ddpzp/autotask/AutoTaskApplication.java
@@ -2,8 +2,10 @@ package com.ddpzp.autotask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class AutoTaskApplication {
 
 	public static void main(String[] args) {
